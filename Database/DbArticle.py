77a0db37fe2dd082db0986f57c22eecb58fbf4df
KennethFlye <10434642@ucn.dk>
2023-10-16 12:09:46 +0200

    basic gui updated

--- Database/DbArticle.py
+++ Database/DbArticle.py
@@ -10,6 +10,22 @@ class DbArticle:
         connection = pymssql.connect(config('SERVER'), config('USER'), config('PASSWORD'), config('CATALOG'))
         self.cursor = connection.cursor(as_dict=True)
 
+
+    def insertArticles(self, articleList):
+        for article in articleList:
+            self.addSubmitter(article.submitter)
+            pass
+            #Tjekkes efter om der findes submitter i databasen, hvis ikke opret den
+            #Tjekkes efter om alle forfatterne findes i databasen, hvis ikke opret dem
+            #Tilføj de kommentar der er til databasen
+            #Tjek om kategorierne findes i databasen, hvis ikke opret dem
+            #Tjek om Authors Parsed findes i databasen, hvis ikke tilføj dem
+
+            #Tilføj artiklen
+
+    def addSubmitter(self, submitter):
+        pass
+
     def TestDatabase(self):
         self.cursor.execute('SELECT * FROM Author')
         for row in self.cursor:
--- GUI/MainMenu.py
+++ GUI/MainMenu.py
@@ -7,24 +7,30 @@ class MainMenu:
         self.root = tk.Tk()
         self.root.title("Pure Merge")
 
-    def create_row(self, parent, label_text_type, label_text, button1_text, button2_text, label2_text):
-        frame = tk.Frame(parent)
-        frame.pack(fill=tk.X)
+    def create_row(self, parent, label_text_type, label_text, label2_text):
+        #frame = tk.Frame(parent)
+        #frame.pack(fill=tk.X)
 
-        labelType = tk.Label(frame, text=label_text_type, width=15, anchor='w')
-        labelType.pack(side=tk.LEFT)
+        current_row = len(parent.grid_slaves())
 
-        label = tk.Label(frame, text=label_text, wraplength=400, anchor='w')
-        label.pack(side=tk.LEFT)
+        labelType = tk.Label(parent, text=label_text_type)
+        labelType.grid(row=current_row, column=0, sticky='w')
 
-        button1 = tk.Button(frame, text=button1_text)
-        button1.pack(side=tk.LEFT)
+        label = tk.Label(parent, text=label_text, wraplength=1000)
+        label.grid(row=current_row, column=1)
 
-        button2 = tk.Button(frame, text=button2_text)
-        button2.pack(side=tk.LEFT)
+        #button1 = tk.Button(parent, text=button1_text)
+        isPressed1 = tk.BooleanVar()
+        button1 = tk.Checkbutton(parent, variable=isPressed1)
+        button1.grid(row=current_row, column=2)
 
-        label2 = tk.Label(frame, text=label2_text, wraplength=400, anchor='w')
-        label2.pack(side=tk.LEFT)
+        #button2 = tk.Button(parent, text=button2_text)
+        isPressed2 = tk.BooleanVar()
+        button2 = tk.Checkbutton(parent, variable=isPressed2)
+        button2.grid(row=current_row, column=3)
+
+        label2 = tk.Label(parent, text=label2_text, wraplength=1000)
+        label2.grid(row=current_row, column=4, sticky='w')
 
     def setup(self, articleList):
         articleVariables = articleList[1].getListOfVariables()
@@ -34,6 +40,23 @@ class MainMenu:
             article1 = articleList[counter]
             article2 = articleList[counter+1]
             variable = articleVariables[i]
-            self.create_row(self.root, f'{variable}:',  f'{getattr(article1, variable) }', f'1', f'2', f'{getattr(article2, variable)}')
+            self.create_row(self.root, f'{variable}:',  f'{getattr(article1, variable) }', f'{getattr(article2, variable)}')
+
+        self.create_merge_row(self.root)
 
         self.root.mainloop()
+
+    def create_merge_row(self, parent):
+        current_row = len(parent.grid_slaves())
+
+        buttonAccept = tk.Button(parent, text='Accept Merge', bg='green', command=self.buttonAccept)
+        buttonAccept.grid(row=current_row, column=0, sticky='w')
+
+        buttonCancel = tk.Button(parent, text='Cancel Merge', bg='red', command=self.buttonCancel)
+        buttonCancel.grid(row=current_row, column=1, sticky='w')
+
+    def buttonAccept(self):
+        pass
+
+    def buttonCancel(self):
+        pass
